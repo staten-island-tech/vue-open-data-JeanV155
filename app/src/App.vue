a865488c32f889400bde3e48acc2bc2b3cda55a4
JeanV155 <carlosgutierrez203074@gmail.com>
2026-03-16 12:32:35 -0400

    eeee

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,27 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+async function getData() {
+  const url = "https://data.cityofnewyork.us/resource/bmxf-3rd4.json";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
 </script>
 
-<template>
-  <HelloWorld />
-</template>
+<style  scoped>
+
+</style>
